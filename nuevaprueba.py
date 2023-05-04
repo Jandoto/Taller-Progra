a3f0760c625cca51d61f9ba22af225c9b3ca042e
Jandoto <alejandro.garcia@nos.cl>
2023-05-04 02:54:53 -0400

    assas

--- nuevaprueba.py
+++ nuevaprueba.py
@@ -15,14 +15,15 @@ class Matrix:
             raise ValueError("El número de valores proporcionados no coincide con las dimensiones de la matriz.")
     
     def __str__(self):
-        matrix_str = ''
-        for fila in self.matriz:
-            matrix_str += '| '
-            for elemento in fila:
-                matrix_str += f'{elemento} '
-            matrix_str = matrix_str.rstrip()  
-            matrix_str += ' |\n'
-        return matrix_str.rstrip()  
+        output = []
+        max_len = max(len(str(x)) for row in self.matriz for x in row)
+
+        for row in self.matriz:
+            formatted_row = [f'{x:<{max_len}}' for x in row]
+            row_str = '| ' + ' '.join(formatted_row) + ' |'
+            output.append(row_str)
+
+        return '\n'.join(output)
     
     def __add__(self,b):
         if self.y != b.y or self.z != b.z:
@@ -44,6 +45,25 @@ class Matrix:
                 valores.append(valor)
         return Matrix(valores,self.y,self.z)
     
+    def fusion_matrices(self, matriz_alpha):
+        if self.y != matriz_alpha.y or self.z != matriz_alpha.z:
+            raise ValueError("Las matrices deben tener las mismas dimensiones para fusionarlas.")
+
+        fusionada = []
+        for i in range(self.y):
+            for j in range(self.z):
+                valor_numerico = str(self.matriz[i][j])
+                valor_alfabetico = matriz_alpha.matriz[i][j]
+                if valor_alfabetico:
+                    fusion = valor_numerico + "+" + valor_alfabetico
+                    print(fusion)
+                else:
+                    fusion = valor_numerico
+                fusionada.append(fusion)
+        
+        return Matrix(fusionada, self.y, self.z)
+    
+
     def separar_letras_numeros(self,l1):
         resultados = []
         for elemento in l1:
@@ -155,16 +175,13 @@ class Matrix:
         return Matrix(valores,self.y,self.z)
     
     def __mul__(self, b):
-        if self.z != b.y:
-            raise ValueError("El número de columnas de la primera matriz debe coincidir con el número de filas de la segunda matriz.")
         valores = []
         for i in range(self.y):
-            for j in range(b.z):
-                valor = 0
-                for k in range(self.z):
-                    valor += self.matriz[i][k] * b.matriz[k][j]
+            for j in range(self.z):
+                valor = self.matriz[i][j] * b
+                print(valor)
                 valores.append(valor)
-        return Matrix(valores, self.y, b.z)
+        return Matrix(valores, self.y, self.z)
     
     def mul2(self, a,b,c):
         ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -176,9 +193,11 @@ class Matrix:
                 valor = 0
                 val_alpha=""
                 for k in range(self.z):
-                    valor += self.matriz[i][k] * a.matriz[k][j] #num con num
-                    print(valor,"primero")
-                    #Num con alfa
+                    #MULTIPLICACION PARTE NÚMERICA CON NÚMERICA
+                    valor += self.matriz[i][k] * a.matriz[k][j] 
+
+                    
+                    #MULTIPLICACION PARTE NÚMERICA CON ALFABETICA
                     valor1=c.matriz[k][j]
                     valor1=valor1.replace(" ","").split("+")
                     lista1 = self.separar_letras_numeros(valor1)
@@ -186,18 +205,59 @@ class Matrix:
                     val=""
                     for t in range (len(lista1)):
                         if lista1[t]!=0:
-                            print("entra")
+
                             lista1[t]=lista1[t]*int(self.matriz[i][k])
 
                             if lista1[t]!=0:
-                                val=val+str(lista1[t])+str(ABC[t])
+                                if lista1[t]==1:
 
-                                val=val+"+"
+                                    val=val+str(ABC[t])
+                                    val=val+"+"
+                                else:
+                                    val=val+str(lista1[t])+str(ABC[t])
+                                    val=val+"+"
+
                         else:
                             pass
                     val_alpha=val_alpha+val
-                    #b.matriz[i][k]*c.matriz[k][j] #alfa con alfa
+                    
 
+                    #MULTIPLICACION PARTE ALFABETICA CON ALFABETICA
+                    valor1=b.matriz[i][k]
+                    valor1=valor1.replace(" ","").split("+")
+                    lista1 = self.separar_letras_numeros(valor1)
+
+                    valor2=c.matriz[k][j]
+                    valor2=valor2.replace(" ","").split("+")
+                    lista2 = self.separar_letras_numeros(valor2)
+
+                    val=""
+                    auxiliar=[]
+                    for t in range (len(lista1)):
+                        if lista1[t]!=0:
+                            
+                            for g in range (len(lista2)):
+                                if lista2[g]!=0:
+
+                                    lista1[t]=lista1[t]*lista2[g]
+                                    if lista1[t]==1:
+                                        val=val+str(ABC[t])+str(ABC[g])
+                                        val=val+"+"
+                                    else:
+                                        val=val+str(lista1[t])+str(ABC[t])+str(ABC[g])
+                                        val=val+"+"
+
+
+                            #if lista1[t]!=0:
+                                #val=val+str(lista1[t])+str(ABC[t])+str(ABC[g])
+
+                                #val=val+"+"
+                        else:
+                            pass
+                    val_alpha=val_alpha+val
+
+
+                    #MULTIPLICACION PARTE ALFABETICA CON NÚMERICA
                     valor1=b.matriz[i][k]
                     valor1=valor1.replace(" ","").split("+")
                     lista1 = self.separar_letras_numeros(valor1)
@@ -209,15 +269,31 @@ class Matrix:
                             lista1[h]=lista1[h]*int(a.matriz[k][j])
 
                             if lista1[h]!=0:
-                                val=val+str(lista1[h])+str(ABC[h])
+                                if lista1[h]==1:
 
-                                val=val+"+"
+                                    val=val+str(ABC[h])
+                                    val=val+"+"
+                                else:
+                                    val=val+str(lista1[h])+str(ABC[h])
+                                    val=val+"+"
                         else:
                             pass
-                    val_alpha=val_alpha+val[:-1]
-                valor=str(valor)+"+"+val_alpha
-                print(valor,"segundo")
+                    val_alpha=val_alpha+val
+                    ########################################################
+                
+
+                if val_alpha.endswith('+'):
+                    val_alpha=val_alpha[:-1]
+                else:
+                    pass
+
+                
+                if val_alpha != "":         
+                    valor=str(valor)+"+"+val_alpha
+                else:
+                    valor=str(valor)
                 valores.append(valor)
+
         return Matrix(valores, self.y, a.z)
     
     def mul_alpha(self,b):
@@ -258,25 +334,28 @@ class Matrix:
 ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 matrices = []
 matrices_alpha=[]
+matrices_total=[]
 for i in range(len(ABC)):
     matrices.append("NULL")
     matrices_alpha.append("NULL")
+    matrices_total.append("NULL")
 n=0
 
 
-valores1 = [1, 2, 2, 2, 2, 2]
+valores1 = [1, 2, 2, 1, 1, 2]
 valores4 = ["2A", "", "", "", "", ""]
+valores5 = ["B", "", "", "", "", ""]
 y1 = 2
 z1 = 3
 matrices[0] = Matrix(valores1, y1, z1)
 matrices_alpha[0] = Matrix(valores4, y1, z1)
 
 a=Matrix.transpuesta(matrices[0])
-valores2 = [1, 2, 2, 2, 2, 2]
-y2 = 3
-z2 = 2
+valores2 = [1, 2, 2, 1, 1, 2]
+y2 = 2
+z2 = 3
 matrices[1] = Matrix(valores2, y2, z2)
-matrices_alpha[1] = Matrix(valores4, y2, z2)
+matrices_alpha[1] = Matrix(valores5, y2, z2)
 
 while n != "SALIR":
     n=input("Hola :")
@@ -292,6 +371,7 @@ while n != "SALIR":
         
             matrices[valor] = matrices[valor1] + matrices[valor2]
             matrices_alpha[valor] = matrices_alpha[valor1].suma_alpha(matrices_alpha[valor2])
+            matrices_total[valor] = matrices[valor].fusion_matrices(matrices_alpha[valor])
 
 
 
@@ -309,18 +389,20 @@ while n != "SALIR":
             valor=ABC.index(n[0])
             valor1=n[2]
             valor2=ABC.index(n[4])
-        
             
+            matrices[valor] = matrices[valor2] * int(valor1)  
             matrices_alpha[valor] = matrices_alpha[valor2].mul_alpha(valor1)
+            matrices_total[valor] = matrices[valor].fusion_matrices(matrices_alpha[valor])
 
-        if n[3]=="*":
+        if n[3]=="*" and n[2] in ABC and n[4]in ABC:
+            print("AAAAA")
             valor=ABC.index(n[0])
             valor1=ABC.index(n[2])
             valor2=ABC.index(n[4])
 
         
-            matrices[valor] = matrices[valor1] * matrices[valor2]
-            matrices_alpha[valor] = matrices[valor1].mul2(matrices[valor2],matrices_alpha[valor1],matrices_alpha[valor2])
+   
+            matrices_total[valor] = matrices[valor1].mul2(matrices[valor2],matrices_alpha[valor1],matrices_alpha[valor2])
 
         if "**T" in n:
             valor=ABC.index(n[0])
@@ -328,7 +410,8 @@ while n != "SALIR":
             print(valor)
         
             matrices[valor] = Matrix.transpuesta(matrices[valor1])
-            matrices_alpha[valor] = Matrix.transpuesta(matrices_alpha[valor1]) 
+            matrices_alpha[valor] = Matrix.transpuesta(matrices_alpha[valor1])
+            matrices_total[valor] = matrices[valor].fusion_matrices(matrices_alpha[valor]) 
 
         if "sort" in n:
             print("sort")
@@ -338,5 +421,6 @@ while n != "SALIR":
     elif n[0]=="s" and n[1]=="o":
         print("SSSSSSSSSSSSSS")
 
-    print(matrices_alpha[2])
+    print(matrices[2])
+    print(matrices_total[2])
 
